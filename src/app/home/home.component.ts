13bcf5923fbb1548534bdca1ea95f1757e9425cf
babarshaheryar <babarshaheryar20@gmail.com>
2024-01-22 17:46:05 +0500

    universal downloader front side

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,78 @@
+import { Component, OnInit } from "@angular/core";
+import { NgbModal, ModalDismissReasons } from "@ng-bootstrap/ng-bootstrap";
+import { NgbDate, NgbCalendar } from "@ng-bootstrap/ng-bootstrap";
+
+@Component({
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.scss"],
+})
+export class HomeComponent implements OnInit {
+  model = {
+    left: true,
+    middle: false,
+    right: false,
+  };
+
+  focus;
+  focus1;
+  fromDate: NgbDate;
+  toDate: NgbDate;
+  closeResult: string;
+
+  constructor(private modalService: NgbModal, calendar: NgbCalendar) {
+    this.fromDate = calendar.getToday();
+    this.toDate = calendar.getNext(calendar.getToday(), "d", 10);
+  }
+
+  open(content, type, modalDimension) {
+    if (modalDimension === "sm" && type === "modal_mini") {
+      this.modalService
+        .open(content, {
+          windowClass: "modal-mini",
+          size: "sm",
+          centered: true,
+        })
+        .result.then(
+          (result) => {
+            this.closeResult = `Closed with: ${result}`;
+          },
+          (reason) => {
+            this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+          }
+        );
+    } else if (modalDimension === "" && type === "Notification") {
+      this.modalService
+        .open(content, { windowClass: "modal-danger", centered: true })
+        .result.then(
+          (result) => {
+            this.closeResult = `Closed with: ${result}`;
+          },
+          (reason) => {
+            this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+          }
+        );
+    } else {
+      this.modalService.open(content, { centered: true }).result.then(
+        (result) => {
+          this.closeResult = `Closed with: ${result}`;
+        },
+        (reason) => {
+          this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+        }
+      );
+    }
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return "by pressing ESC";
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return "by clicking on a backdrop";
+    } else {
+      return `with: ${reason}`;
+    }
+  }
+
+  ngOnInit() {}
+}
